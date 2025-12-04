f817d7b3cefe2e7cfd42ffce0c3c1682376e7cb8
Mauricio Oliveira <msinstalacao@gmail.com>
2025-12-04 09:27:15 -0300

    Instalado models e BD

---
+++ app/model/ContentScheduling.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ContentScheduling extends Pivot
+{
+    //
+}
---
+++ app/model/OrganizationUser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\model;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class OrganizationUser extends Pivot
+{
+    //
+}
